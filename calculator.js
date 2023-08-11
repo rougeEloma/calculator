af4b4f77d2a7ef4c5639de1b554885039514c6f4
alain eloma <alainelomapro@gmail.com>
2023-08-11 09:57:36 +0100

    imported all the html balises

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 // Do not remove this function definition and its export
 // this function should return the result of the current operation
 function calculate(expression) {
+  
   return expression;
 }
 
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-{ calculate } import './calculator';
+// { calculate } import './calculator';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
+
+var inputUser = document.getElementById('input'), // input/output button
+reset = document.getElementById('reset'), // AC button
+clear = document.getElementById('clear'), // clear button
+plusoumoins = document.getElementById('plusoumoins'), // plusaumoins button
+percentage = document.getElementById('percentage'), // percentage button
+divideby = document.getElementById('divideby'), // divideby button
+times = document.getElementById('times'), // times button
+minus = document.getElementById('minus'), // minus button
+plus = document.getElementById('plus'), // plus button
+  theNumber = document.querySelectorAll('.numpad'), // number buttons (dot included)
+  theDot = document.querySelectorAll('.dot'), // dot buttons 
+  equals = document.getElementById('equals'); // equal button
