314d453a81510b2aab896f2af30832686ddca685
Alain ELOMA <alainelomapro@gmail.com>
2023-08-16 21:20:35 +0100

    elements of the DOM HTML

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-// { calculate } import './calculator';
+import { calculate } from "./calculator.js";
 
 // TODO: Faire la manipulation du DOM dans ce fichier
 
-var inputUser = document.getElementById('input'), // input/output button
-reset = document.getElementById('reset'), // AC button
-clear = document.getElementById('clear'), // clear button
-plusoumoins = document.getElementById('plusoumoins'), // plusaumoins button
-percentage = document.getElementById('percentage'), // percentage button
-divideby = document.getElementById('divideby'), // divideby button
-times = document.getElementById('times'), // times button
-minus = document.getElementById('minus'), // minus button
-plus = document.getElementById('plus'), // plus button
-  theNumber = document.querySelectorAll('.numpad'), // number buttons (dot included)
-  theDot = document.querySelectorAll('.dot'), // dot buttons 
-  equals = document.getElementById('equals'); // equal button
+// elements of the DOM HTML
+const label = document.querySelector('#calcul'),
+currentInput = document.querySelector('#input'),
+buttons = document.querySelectorAll()
+
+// containers of each expressions
+let expression = ''; // might not need it
+let operator = '';
+
+buttons.forEach(button => {
+  button.addEventListener('click', event => {
+
+    const buttonContent = button.textContent;
+    
+    if (buttonContent >= '0' && buttonContent <= '9') {
+
+    }
+    else if (buttonContent === '.') {
+
+    }
+    else {
+      switch (buttonContent) {
+        case 'AC' :
+          label.innerText = "";
+          currentInput.value = "";
+        case 'C' :
+          
+      }
+    }
+    
+  })
+})
