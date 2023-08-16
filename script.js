ffc332b9160433fd0ec27cafbcea7f12f2ac84ff
Alain ELOMA <alainelomapro@gmail.com>
2023-08-16 21:22:42 +0100

    begining of the forEach buttons : AC and C

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ buttons = document.querySelectorAll()
 let expression = ''; // might not need it
 let operator = '';
 
+buttons.forEach(button => {
+  button.addEventListener('click', event => {
+
+    const buttonContent = button.textContent;
+    
+    if (buttonContent >= '0' && buttonContent <= '9') {
+
+    }
+    else if (buttonContent === '.') {
+
+    }
+    else {
+      switch (buttonContent) {
+        case 'AC' :
+          label.innerText = "";
+          currentInput.value = "";
+        case 'C' :
+          
+      }
+    }
+    
+  })
+})
