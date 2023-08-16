d226ed82d89d6b2fdc717618f3a1dacb775dff25
Alain ELOMA <alainelomapro@gmail.com>
2023-08-16 23:04:54 +0100

    adding digits in the current input

--- script.js
+++ script.js
@@ -5,30 +5,57 @@ import { calculate } from "./calculator.js";
 // elements of the DOM HTML
 const label = document.querySelector('#calcul'),
 currentInput = document.querySelector('#input'),
-buttons = document.querySelectorAll()
+buttons = document.querySelectorAll('button');
 
-// containers of each expressions
+// const inputValue = currentInput.value;
+
+// containers of each labels
 let expression = ''; // might not need it
 let operator = '';
 
 buttons.forEach(button => {
-  button.addEventListener('click', event => {
+  button.addEventListener('click', (event) => {
 
     const buttonContent = button.textContent;
     
     if (buttonContent >= '0' && buttonContent <= '9') {
-
+      if (buttonContent === '0') {
+        if (currentInput.value.length === 1 && currentInput.value === '0') {
+          currentInput.value += '';
+        } 
+        else {
+          currentInput.value += buttonContent;
+        }
+      }
+      else {
+        currentInput.value += buttonContent;
+      }
     }
     else if (buttonContent === '.') {
-
+      if (currentInput.value.length === 0) {
+        currentInput.value += '';
+      }
+      else if (currentInput.value.includes(buttonContent)) {
+        currentInput.value += '';
+      }
+      else {
+        currentInput.value += buttonContent;
+      }
     }
     else {
       switch (buttonContent) {
         case 'AC' :
           label.innerText = "";
           currentInput.value = "";
+
+          event.preventDefault();
+          break;
         case 'C' :
+          currentInput.value = currentInput.value.slice(0, -1);
+          label = label.slice(0, -1);
           
+          event.preventDefault();
+          break;
       }
     }
     
